d5495dbfad199125c35f307807f83d2dbe94066b
fazliddinofficial <fazliddinquvatboyev8@gmail.com>
2025-04-20 19:09:56 +0500

    setup project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Main from "./components/header";
+
+function App() {
+
+  return (
+  <>
+    <Main />  
+  </>
+)
+}
+
+export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+function Main() {
+  return (
+    <>
+      <h1>shit</h1>
+    </>
+  );
+}
+
+export default Main;
